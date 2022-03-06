464e8e582908d5865ec115add4ece528ea2e71b4
rebeca <rbk.alex1997@gmail.com>
2022-03-05 20:44:59 -0600

    mi segundo proyecto

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,12 +1,83 @@
-import { example, anotherExample } from '../src/data.js';
+import { filtro , ordenarAZ } from '../src/data.js';
+//import  results  from '../src/mockData.js';
 
 
-describe('example', () => {
-  it('is a function', () => {
-    expect(typeof example).toBe('function');
+
+describe('test para ordenar data', () => {
+  test('que sea una funcion', () => {
+    expect(typeof ordenarAZ).toBe('function');
+    })
+  test('debe ordenar de A-Z', () => {
+    const palabras = ["manzana", "uva", "girasol", "abeja"]
+   // console.log(ordenarAZ(palabras));
+    expect(ordenarAZ(palabras)).toStrictEqual(['abeja', 'girasol', 'manzana', 'uva']);
+    })  
   });
+  
+  describe('test para filtar data', () => {
+    test('que sea una funcion', () => {
+      expect(typeof filtro).toBe('function');
+    })
+        test('retorna el nombre de un obj', () =>{
+          const obj =  {
+            "id": 1,
+            "name": "Rick Sanchez",
+            "status": "Alive",
+            "species": "Human",
+            "type": "",
+            "gender": "Male",
+            "origin": {
+                "name": "Earth (C-137)",
+                "url": "https://rickandmortyapi.com/api/location/1"
+            },
+            "location": {
+                "name": "Earth (Replacement Dimension)",
+                "url": "https://rickandmortyapi.com/api/location/20"
+            },
+            "image": "https://raw.githubusercontent.com/Laboratoria/rick-and-morty-images/master/images/1.jpeg",
+            "episode": [
+                "https://rickandmortyapi.com/api/episode/1",
+                "https://rickandmortyapi.com/api/episode/2",
+                "https://rickandmortyapi.com/api/episode/3",
+                "https://rickandmortyapi.com/api/episode/4",
+                "https://rickandmortyapi.com/api/episode/5",
+                "https://rickandmortyapi.com/api/episode/6",
+                "https://rickandmortyapi.com/api/episode/7",
+                "https://rickandmortyapi.com/api/episode/8",
+                "https://rickandmortyapi.com/api/episode/9",
+                "https://rickandmortyapi.com/api/episode/10",
+                "https://rickandmortyapi.com/api/episode/11",
+                "https://rickandmortyapi.com/api/episode/12",
+                "https://rickandmortyapi.com/api/episode/13",
+                "https://rickandmortyapi.com/api/episode/14",
+                "https://rickandmortyapi.com/api/episode/15",
+                "https://rickandmortyapi.com/api/episode/16",
+                "https://rickandmortyapi.com/api/episode/17",
+                "https://rickandmortyapi.com/api/episode/18",
+                "https://rickandmortyapi.com/api/episode/19",
+                "https://rickandmortyapi.com/api/episode/20",
+                "https://rickandmortyapi.com/api/episode/21",
+                "https://rickandmortyapi.com/api/episode/22",
+                "https://rickandmortyapi.com/api/episode/23",
+                "https://rickandmortyapi.com/api/episode/24",
+                "https://rickandmortyapi.com/api/episode/25",
+                "https://rickandmortyapi.com/api/episode/26",
+                "https://rickandmortyapi.com/api/episode/27",
+                "https://rickandmortyapi.com/api/episode/28",
+                "https://rickandmortyapi.com/api/episode/29",
+                "https://rickandmortyapi.com/api/episode/30",
+                "https://rickandmortyapi.com/api/episode/31"
+            ],
+            "url": "https://rickandmortyapi.com/api/character/1",
+            "created": "2017-11-04T18:48:46.250Z"
+        }; 
+        expect(obj.name).toEqual('Rick Sanchez');
+        });
+  });
+  
 
-  it('returns `example`', () => {
+
+/* it('returns `example`', () => {
     expect(example()).toBe('example');
   });
 });
@@ -19,5 +90,4 @@ describe('anotherExample', () => {
 
   it('returns `anotherExample`', () => {
     expect(anotherExample()).toBe('OMG');
-  });
-});
+  });*/
